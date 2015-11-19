fd473858522aded44ee901cc552aa668d2c212da
BuildTools <unconfigured@null.spigotmc.org>
2015-11-18 21:29:34 -0500

    - Initial submission

---
+++ LoveFisher/src/LoveFisher.java
@@ -0,0 +1,37 @@
+import org.dreambot.api.methods.Calculations;
+
+import com.xboxnolifes.love.framework.script.*;
+
+
+public class LoveFisher extends Script{
+
+
+	@Override
+	public void onStart() {
+		log("[INFO]: LoveFisher has started.");
+		log("[INFO]: If you notice any bug, please report them on the forums");
+		log("[INFO]: http://dreambot.org/forums/");
+	}
+
+	@Override
+	public void onExit() {
+		log("[INFO]: LoveFisher is stopping");
+		log("[INFO]: Hope you use my scripts again XD - xboxnolifes");
+	}
+	
+	@Override
+	public int onLoop() {
+		
+		switch(State.getState()){
+		case FISHING:
+			break;
+		case DROPPING:
+			break;
+		default:
+			break;
+		}
+		
+		return Calculations.random(163, 1333);
+	}
+
+}
